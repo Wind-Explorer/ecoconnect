824875a8a5f61de86e0d594e7adac109b452e8b7
Wind-Explorer <windexplorer@outlook.com>
2024-06-19 23:18:34 +0800

    Prepped server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
 
 const app = express();
-const PORT = 3000;
+let PORT = process.env.APP_PORT;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
