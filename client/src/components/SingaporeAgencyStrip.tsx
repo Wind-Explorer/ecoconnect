7207efa2da38cb35f003cbd1c28abc663e66ca24
Wind-Explorer <windexplorer@outlook.com>
2024-07-23 12:26:26 +0800

    SAS CSS

--- client/src/components/SingaporeAgencyStrip.tsx
+++ client/src/components/SingaporeAgencyStrip.tsx
@@ -2,8 +2,8 @@ import { Link } from "@nextui-org/react";
 
 export default function SingaporeAgencyStrip() {
   return (
-    <div className="h-8 bg-neutral-200 text-black relative">
-      <div className="mx-auto  w-full flex flex-row gap-2 *:my-auto h-full text-sm max-w-7xl pl-4">
+    <div className="bg-neutral-200 text-black relative p-1">
+      <div className="mx-auto w-full flex flex-row gap-2 *:my-auto h-full text-sm max-w-7xl pl-4">
         <img src="../assets/Merlion.svg" alt="Merlion Icon" className="w-6" />
         <p>A Singapore Government Agency Website</p>
         <Link size="sm" className="text-blue-500">
